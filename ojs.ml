8bebcef0c36e8479fd47360de5e785eeddab4586
Sebastien Briais <sebastien.briais@lexifi.com>
2015-07-01 16:18:30 +0200

    Fix access to joo_global_object.

--- ojs.ml
+++ ojs.ml
@@ -49,7 +49,7 @@ let undefined = pure_js_expr "undefined"
 
 external equals: t -> t -> bool = "caml_js_equals"
 
-let global = variable "joo_global_object"
+let global = pure_js_expr "joo_global_object"
 
 external new_obj: t -> t array -> t = "caml_js_new"
 
