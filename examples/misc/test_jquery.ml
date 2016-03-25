7c2be81441110d8e404e70e9dd0add25d772da6a
Alain Frisch <alain@frisch.fr>
2016-03-25 14:45:48 +0100

    Extend jquery example.

--- examples/misc/test_jquery.ml
+++ examples/misc/test_jquery.ml
@@ -11,12 +11,27 @@ val alert: string -> unit
 
 let ( !! ) = Jquery.selector
 
+let block s ?text ?(classes = []) ?(ons = []) ?(css = []) ?(props = []) children =
+  let element = Jquery.selector (Printf.sprintf "<%s>" s) in
+  begin match text with
+  | None -> ()
+  | Some text -> Jquery.set_text element text
+  end;
+  List.iter (fun c -> Jquery.add_class element c) classes;
+  List.iter (fun (key, value) -> Jquery.set_css_value element key value) css;
+  List.iter (fun (key, value) -> Jquery.set_prop element key value) props;
+  List.iter (fun (event, f) -> Jquery.on element event f) ons;
+  begin match children with
+    | [] -> ()
+    | _ :: _-> Jquery.append element children
+  end;
+  element
+
 let ajax_test () =
   let open Ajax in
   let complete h = function
     | "success" ->
-        let pre = !!"<pre>" in
-        set_text pre (response_text h);
+        let pre = block "pre" ~text:(response_text h) [] in
         hide pre;
         append !!"body" [pre];
         fade_in pre ~duration:2000
@@ -27,6 +42,7 @@ let ajax_test () =
     | status -> alert (Printf.sprintf "status = %s" status)
   in
   run (settings ~meth:`GET ~url:"test_jquery.ml" ~data_type:"text" ~complete ())
+
 
 let on_ready () =
   let main = !!"#main" in
@@ -50,14 +66,15 @@ let on_ready () =
   in
   on main "click" on_click;
 
-  let div = !! "<div>" in
-  let input = !! "<input>" in
+  let div = block "div" [] in
+  let input = block "input" [] in
+  on input "input" (fun _ -> set_text div (get_val input));
   append main [input; div];
 
-  on input "input" (fun _ -> set_text div (get_val input));
-
-  let btn = !! "<button>SHOW SOURCE CODE</button>" in
-  on btn "click" (fun _ -> ajax_test ());
+  let btn =
+    block "button" ~text:"SHOW SOURCE CODE" []
+      ~ons:["click", (fun _ -> ajax_test ())]
+  in
   append main [btn]
 
 let () =
