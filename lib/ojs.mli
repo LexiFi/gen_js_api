fd6eaad628a0b8fb85fe429c2a6dfd7a9e12d61a
Jérôme Vouillon <jerome.vouillon@gmail.com>
2022-12-08 11:46:15 +0100

    Wrap functions supplied to Ojs.iter_properties
    To support effect handlers, we are changing the calling convention of
    OCaml functions. See https://github.com/ocsigen/js_of_ocaml/pull/1340.
    Wrapping Ocaml functions is now mandatory to use them from JavaScript.

--- lib/ojs.mli
+++ lib/ojs.mli
@@ -89,7 +89,7 @@ external obj: (string * t) array -> t = "caml_js_object"
 val empty_obj: unit -> t
 
 val has_property: t -> string -> bool
-external iter_properties: t -> (string -> unit) -> unit = "caml_ojs_iterate_properties"
+val iter_properties: t -> (string -> unit) -> unit
 
 (** {2 Calling JS functions} *)
 
